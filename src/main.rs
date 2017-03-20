5742b3a37b2fe9070b9c0ec6734f75f2f2f9bc9d
Jonathan Turner <jturner@mozilla.com>
2017-03-20 22:35:22 +1300

    simplify sample/sample-rate

--- src/main.rs
+++ src/main.rs
@@ -13,29 +13,17 @@ extern crate hound;
 
 #[derive(Debug)]
 struct Stream {
-    frame_size: u32,
     frames_per_sec: u32,
 }
 
 #[derive(Debug)]
 struct FrameBuffer {
-    frame_size: u32,
     frames_per_sec: u32,
-    frame: Vec<f32>,
+    frame: f32,
 }
-
-#[derive(Debug)]
-struct DelayIterator {
-    frame_size: u32,
-    frames_per_sec: u32,
-    phase: u32,
-    delay: u32, // delay in # samples
-}
-
 
 #[derive(Debug)]
 struct SineIterator {
-    frame_size: u32,
     frames_per_sec: u32,
     phase: u32,
     amp: f32,
@@ -48,38 +36,29 @@ impl Iterator for SineIterator {
     fn next(&mut self) -> Option<Self::Item> {
         use std::f32::consts::PI;
 
-        let mut fb = FrameBuffer {
-            frame_size: self.frame_size,
+        let t = self.phase as f32 / self.frames_per_sec as f32;
+
+        let frame = (t * self.freq * 2.0 * PI).sin() * self.amp;
+
+        self.phase += 1;
+        self.phase %= self.frames_per_sec;
+
+        Some(FrameBuffer {
             frames_per_sec: self.frames_per_sec,
-            frame: Vec::new(),
-        };
-
-        let sample_rate = self.frame_size * self.frames_per_sec;
-
-        for t in (self.phase .. (self.phase + self.frame_size))
-                                .map(|x| x as f32 / sample_rate as f32) {
-            let sample = (t * self.freq * 2.0 * PI).sin();
-            fb.frame.push(self.amp * sample);
-        }
-
-        self.phase += self.frame_size;
-        self.phase = self.phase % sample_rate;
-
-        Some(fb)
+            frame: frame,
+        })
     }
 }
 
 impl Stream {
-    fn new(frame_size: u32, frames_per_sec: u32) -> Self {
+    fn new(frames_per_sec: u32) -> Self {
         Stream {
-            frame_size: frame_size,
             frames_per_sec: frames_per_sec,
         }
     }
 
     fn sine(&self, freq: f32, amp: f32) -> SineIterator {
         SineIterator {
-            frame_size: self.frame_size,
             frames_per_sec: self.frames_per_sec,
             phase: 0,
             amp: amp,
@@ -92,11 +71,13 @@ impl Stream {
 fn main() {
     use std::i16;
 
-    let output = Stream::new(100, 441);
+    const SAMPLE_RATE:u32 = 44100;
+
+    let output = Stream::new(SAMPLE_RATE);
     
     let spec = hound::WavSpec {
         channels: 1,
-        sample_rate: 44100,
+        sample_rate: SAMPLE_RATE,
         bits_per_sample: 16,
         sample_format: hound::SampleFormat::Int,
     };
@@ -104,14 +85,13 @@ fn main() {
     let mut writer = hound::WavWriter::create("sine.wav", spec).unwrap();
     let mut num_samples = 0;
 
-    for buffer in output.sine(880.0, 1.0) {
-        for t in buffer.frame {
-            let amplitude = i16::MAX as f32;
-            writer.write_sample((t * amplitude) as i16).unwrap();
-            num_samples += 1;
-            if num_samples == 44100 {
-                return;
-            }            
-        }
+    for buffer in output.sine(440.0, 1.0) {
+        let t = buffer.frame;
+        let amplitude = i16::MAX as f32;
+        writer.write_sample((t * amplitude) as i16).unwrap();
+        num_samples += 1;
+        if num_samples == SAMPLE_RATE {
+            return;
+        }            
     }
 }
