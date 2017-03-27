39f033802ba16646b0f909324cf03af7c91bc704
Jonathan Turner <jturner@mozilla.com>
2017-03-27 18:03:56 +1300

    Add silence generator

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,14 @@ struct Stream {
 }
 
 #[derive(Debug)]
-struct FrameBuffer {
+struct AudioFrame {
     frames_per_sec: u32,
     frame: f32,
 }
 
-impl FrameBuffer {
-    fn new(frames_per_sec: u32, frame: f32) -> FrameBuffer {
-        FrameBuffer {
+impl AudioFrame {
+    fn new(frames_per_sec: u32, frame: f32) -> AudioFrame {
+        AudioFrame {
             frames_per_sec: frames_per_sec,
             frame: frame,
         }
@@ -21,18 +21,34 @@ impl FrameBuffer {
 }
 
 #[derive(Debug)]
-struct SineIterator {
+struct SilenceGenerator {
+    frames_per_sec: u32,
+}
+
+impl Iterator for SilenceGenerator {
+    type Item = AudioFrame;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(AudioFrame {
+            frames_per_sec: self.frames_per_sec,
+            frame: 0.0
+        })
+    }
+}
+
+#[derive(Debug)]
+struct SineGenerator {
     frames_per_sec: u32,
     phase: u32,
     freq: f32,
 }
 
-trait AudioIteratorMethods: Iterator<Item=FrameBuffer>+Sized {
+trait AudioIteratorMethods: Iterator<Item=AudioFrame>+Sized {
     fn amp(self, amplitude: f32)->AmpIterator<Self>;
 }
 
-impl Iterator for SineIterator {
-    type Item = FrameBuffer;
+impl Iterator for SineGenerator {
+    type Item = AudioFrame;
 
     fn next(&mut self) -> Option<Self::Item> {
         use std::f32::consts::PI;
@@ -43,14 +59,14 @@ impl Iterator for SineIterator {
 
         self.phase = (self.phase + 1) % self.frames_per_sec;
 
-        Some(FrameBuffer {
+        Some(AudioFrame {
             frames_per_sec: self.frames_per_sec,
             frame: frame,
         })
     }
 }
 
-impl AudioIteratorMethods for SineIterator {
+impl AudioIteratorMethods for SineGenerator {
     fn amp(self, amplitude: f32)->AmpIterator<Self> {
         AmpIterator {
             frames_per_sec: self.frames_per_sec,
@@ -60,18 +76,18 @@ impl AudioIteratorMethods for SineIterator {
     }
 }
 
-struct AmpIterator<T: Iterator<Item=FrameBuffer>+Sized> {
+struct AmpIterator<T: Iterator<Item=AudioFrame>+Sized> {
     frames_per_sec: u32,
     amplitude: f32,
     iter: T,
 }
 
-impl<T: Iterator<Item=FrameBuffer>+Sized> Iterator for AmpIterator<T> {
-    type Item = FrameBuffer;
+impl<T: Iterator<Item=AudioFrame>+Sized> Iterator for AmpIterator<T> {
+    type Item = AudioFrame;
 
     fn next(&mut self) -> Option<Self::Item> {
         match self.iter.next() {
-            Some(x) => Some(FrameBuffer::new(x.frames_per_sec, x.frame * self.amplitude)),
+            Some(x) => Some(AudioFrame::new(x.frames_per_sec, x.frame * self.amplitude)),
             None => None
         } 
     }
@@ -84,11 +100,17 @@ impl Stream {
         }
     }
 
-    fn sine(&self, freq: f32) -> SineIterator {
-        SineIterator {
+    fn sine(&self, freq: f32) -> SineGenerator {
+        SineGenerator {
             frames_per_sec: self.frames_per_sec,
             phase: 0,
             freq: freq,
+        }
+    }
+
+    fn silence(&self) -> SilenceGenerator {
+        SilenceGenerator {
+            frames_per_sec: self.frames_per_sec
         }
     }
 }
@@ -109,15 +131,20 @@ fn main() {
     };
 
     let mut writer = hound::WavWriter::create("sine.wav", spec).unwrap();
-    let mut num_samples = 0;
 
-    for buffer in output.sine(440.0).amp(0.3) {
+    for buffer in output.sine(440.0).amp(0.3).take(SAMPLE_RATE as usize) {
         let t = buffer.frame;
         let amplitude = i16::MAX as f32;
         writer.write_sample((t * amplitude) as i16).unwrap();
-        num_samples += 1;
-        if num_samples == SAMPLE_RATE {
-            return;
-        }            
+    }
+    for buffer in output.silence().take(SAMPLE_RATE as usize) {
+        let t = buffer.frame;
+        let amplitude = i16::MAX as f32;
+        writer.write_sample((t * amplitude) as i16).unwrap();
+    }
+    for buffer in output.sine(660.0).amp(0.3).take(SAMPLE_RATE as usize) {
+        let t = buffer.frame;
+        let amplitude = i16::MAX as f32;
+        writer.write_sample((t * amplitude) as i16).unwrap();
     }
 }
